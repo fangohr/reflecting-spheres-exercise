c3dd275db29f34ce5c634728b797362c3ca08804
Hans Fangohr <fangohr@soton.ac.uk>
2016-10-03 12:28:16 +0100

    shorter and different variable names

--- bounce.cc
+++ bounce.cc
@@ -1,23 +1,14 @@
 #include <iostream>
 
 
-int minColumn = 0;
-int maxColumn = 80;
-char particleSymbol = 'x';
-double particlePosition = minColumn;
-double particleSpeed = 6.3;
-int timeStep = 0;
-int stopTime = 60;
-
-
-void draw() {
-  for (int i = 0; i < particlePosition; i++) {
+void draw(double pos, char sym) {
+  for (int i = 0; i < pos; i++) {
     std::cout << " ";
   }
-  std::cout << particleSymbol << std::endl;
+  std::cout << sym << std::endl;
 }
 
-void move() {
+void move(double &particlePosition, double &particleSpeed, int maxColumn, int minColumn) {
   particlePosition += particleSpeed;
   if (particlePosition >= maxColumn) {
     particlePosition = maxColumn;
@@ -29,10 +20,17 @@ void move() {
 }
 
 int main() {
+  int minColumn = 0;
+  int maxColumn = 80;
+  char particleSymbol = 'x';
+  double particlePosition = minColumn;
+  double particleSpeed = 6.3;
+  int timeStep = 0;
+  int stopTime = 60;
   
   while (timeStep < stopTime) {
-    draw();
-    move();
+    draw(particlePosition, particleSymbol);
+    move(particlePosition, particleSpeed, maxColumn, minColumn);
     timeStep++;
   }
 }
