8374930754942072f84f6590bd31cf90ac6ba818
Hans Fangohr <fangohr@soton.ac.uk>
2016-10-04 15:40:17 +0100

    Add new test

--- test_bounce.py
+++ test_bounce.py
@@ -20,6 +20,7 @@ def test_output_ignore_whitespace():
     status, command_output = subprocess.getstatusoutput('./bounce > output.txt')
     assert status == 0
     print("command_output = '{}'".format(command_output));
+    
     for line_nr, (ref, cur) in enumerate(zip(reference_output, open("output.txt", 'rt'))):
         print("processing line {}:".format(line_nr));
         if 'x' in cur:
@@ -40,4 +41,20 @@ def test_output_2_particles():
             assert '*' in cur
             assert ref.index('*') == cur.index('*')
         
+
+def test_output_is_nontrivial():
+    status, command_output = subprocess.getstatusoutput('./bounce')
+    assert status == 0
+    print(len(command_output))
+    assert len(command_output) == 4919   # just assume current number is correct
+    n_x = command_output.count('x')
+    print("number of x = {}".format(n_x))
+    assert n_x == 59
+    n_start = command_output.count('*')
+    print("number of * = {}".format(n_start))
+    assert n_start ==60
+
+    # x has one fewer, as it is overwritten by particle 2, which has symbol *
     
+
+
