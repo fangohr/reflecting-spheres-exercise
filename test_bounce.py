25e089838415a2e6645a424dd51292125cc917bd
Hans Fangohr <fangohr@soton.ac.uk>
2016-10-04 15:08:59 +0100

    tidy up of test file

--- test_bounce.py
+++ test_bounce.py
@@ -17,22 +17,21 @@ import pytest
 
 def test_output_ignore_whitespace():
     reference_output = open('bounce_reference_output.txt', 'tr')
-    status, output = subprocess.getstatusoutput('./bounce > output.txt')
+    status, command_output = subprocess.getstatusoutput('./bounce > output.txt')
     assert status == 0
-    print("output = '{}'".format(output));
+    print("command_output = '{}'".format(command_output));
     for line_nr, (ref, cur) in enumerate(zip(reference_output, open("output.txt", 'rt'))):
         print("processing line {}:".format(line_nr));
         if 'x' in cur:
             assert 'x' in ref
             assert ref.index('x') == cur.index('x')
-    
 
-
-    
+            
 def test_output_2_particles():
     reference_output = open('bounce_reference_output-2.txt', 'tr')
-    status, output = subprocess.getstatusoutput('./bounce > output.txt')
-    
+    status, command_output = subprocess.getstatusoutput('./bounce > output.txt')
+    print("command_output = '{}'".format(command_output));
+    assert status == 0
     for ref, cur in zip(reference_output, open('output.txt', 'tr')):
         if 'x' in ref:
             assert 'x' in cur
@@ -41,8 +40,4 @@ def test_output_2_particles():
             assert '*' in cur
             assert ref.index('*') == cur.index('*')
         
-    assert status == 0
-
-
-
     
