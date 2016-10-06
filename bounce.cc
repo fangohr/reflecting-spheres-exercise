516f2f25a61f06ff667cafc36dbf61313cfd830a
Hans Fangohr <fangohr@soton.ac.uk>
2016-10-06 14:22:36 +0100

    separate header and implementation for Screen.

--- bounce.cc
+++ bounce.cc
@@ -13,57 +13,14 @@ class Screen {
 public:
   Screen(int size)
     : buffer(new char[size]), size(size) {}
-
-  Screen(const Screen & other)
-    :  buffer(new char[other.size]), size(other.size)
-  {
-    cout << "Hello from the Screen copy constructor" << endl;
-    std::copy(other.buffer, other.buffer + other.size, this->buffer);
-  }
-
-  Screen& operator=(Screen other) {
-    swap(*this, other);
-    return *this;
-  }
-
+  Screen(const Screen & other);
+  Screen& operator=(Screen other);
   friend void swap(Screen& lhs, Screen& rhs);
-
-  ~Screen() {
-    cout << "Hello from the Screen deconstructor" << endl;
-    delete [] this->buffer;
-  }
-  
-  void put(int const position, char const symbol) {
-    //assert(position < this->size);
-    if (position >= this->size) {
-      cout << "position is " << position << " but screen max column is " << this->size << endl;
-      abort();
-    }
-    if (position < 0) {
-      cout << "position is " << position << " but should be positive" << endl;
-      abort();
-    }
-    
-    this->buffer[position] = symbol;
-  }
-
-  void print() {
-    for (int i = 0; i < this->size; i++) {
-      cout << this->buffer[i];
-    }
-    cout << endl;
-  }
-
-  void clear() {
-    for (int i = 0; i < this->size; i++) {
-      this->buffer[i] = ' ';
-    }
-  }
-
-  char& operator[](unsigned int const pos) {
-    return this->buffer[pos];
-  }
-
+  ~Screen() {delete [] this->buffer;};
+  void put(int const position, char const symbol);
+  void print();
+  void clear(); 
+  char& operator[](unsigned int const pos);
   unsigned int get_size();
 
 private:
@@ -73,8 +30,51 @@ private:
 };
 
 
+Screen::Screen(const Screen & other)
+  :  buffer(new char[other.size]), size(other.size)
+{
+  cout << "Hello from the Screen copy constructor" << endl;
+  std::copy(other.buffer, other.buffer + other.size, this->buffer);
+}
+
 unsigned int Screen::get_size() {
   return this->size;
+}
+
+Screen& Screen::operator=(Screen other) {
+  swap(*this, other);
+  return *this;
+}
+
+void Screen::put(int const position, char const symbol) {
+  //assert(position < this->size);
+  if (position >= this->size) {
+    cout << "position is " << position << " but screen max column is " << this->size << endl;
+    abort();
+  }
+  if (position < 0) {
+    cout << "position is " << position << " but should be positive" << endl;
+    abort();
+  }
+    
+  this->buffer[position] = symbol;
+}
+
+void Screen::print() {
+  for (int i = 0; i < this->size; i++) {
+    cout << this->buffer[i];
+  }
+  cout << endl;
+}
+
+void Screen::clear() {
+  for (int i = 0; i < this->size; i++) {
+    this->buffer[i] = ' ';
+  }
+}
+
+char& Screen::operator[](unsigned int const pos) {
+  return this->buffer[pos];
 }
 
 
