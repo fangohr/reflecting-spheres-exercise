b8530f103d907d0420ba192ffa22ad6652fe5086
Hans Fangohr <fangohr@soton.ac.uk>
2016-10-07 15:46:20 +0100

    removing comments

--- magicparticle.cc
+++ magicparticle.cc
@@ -3,14 +3,9 @@
 void MagicParticle::move() {
   position += speed;
   if (position >= maxColumn) {
-    //position = maxColumn;
     position = minColumn;
-    //
-    //speed = -speed;
   } else if (position < minColumn) {
     position = maxColumn-1;
-    //speed = -speed;
   }
-  //std::cout << "Been in MagicParticle:::move()" << std::endl;
 }
 
